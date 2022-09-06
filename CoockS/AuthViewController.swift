6f8ca8ad9402139805a67f4c5236b5d2c6a825e7
uin <dimayak915@gmail.com>
2022-09-06 20:49:49 +0300

    Добавил авторизацию по логину и паролю Добавил кнопку переключения с регистрации на авторизацию Добавил анимация блюра и загрузки

--- CoockS/AuthViewController.swift
+++ CoockS/AuthViewController.swift
@@ -10,26 +10,44 @@ import UIKit
 import Firebase
 
 class AuthViewController: UIViewController {
-
+    
     var singUp: Bool = true {
         willSet{
             if newValue{
                 titleLabel.text = "Регистрация"
                 emailField.isHidden = false
-                enterButton.setTitle("Войти", for: .normal)
+                enterButton.setTitle("Зарегистрироваться", for: .normal)
+                singButton.setTitle("войти", for: .normal)
+                
             } else {
                 titleLabel.text = "Вход"
                 emailField.isHidden = true
-                enterButton.setTitle("Регистрация", for: .normal)
+                enterButton.setTitle("Войти", for: .normal)
+                singButton.setTitle("Зарегистрироваться", for: .normal)
             }
         }
     }
     
+    let loadingView = UIActivityIndicatorView(style: .large)
+    
+    var blurEffectView = UIVisualEffectView()
+    
+
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var loginField: UITextField!
     @IBOutlet var emailField: UITextField!
     @IBOutlet var passwordField: UITextField!
     @IBOutlet var enterButton: UIButton!
+    @IBOutlet var singButton: UIButton!
+    
+    @IBAction func switchButton(_ sender: UIButton) {
+        switchButtonAction()
+        enterButton.isEnabled = true
+    }
+    
+    @IBAction func switchSingUpIn(_ sender: UIButton) {
+        singUp = !singUp
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,13 +55,114 @@ class AuthViewController: UIViewController {
         loginField.delegate = self
         emailField.delegate = self
         passwordField.delegate = self
+        
+        singUp = false
+        
+        configureBlurView()
+        configureLoadingView()
+        hideLoadingView()
     }
+    
+    
+    
+    // MARK: LOGIC
 
-    @IBAction func switchButton(_ sender: UIButton) {
-        //singUp = !singUp
-        if checkFieldsAndCreateUser() {
-            navigationView()
+    func loginUser(email: String, password: String) {
+
+        showLoadingView()
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+
+            self.hideLoadingView()
+            
+            guard let user = result?.user, error == nil else {
+                print("error: \(error?.localizedDescription ?? "error")")
+                self.showAlert(massage: error?.localizedDescription ?? "localizedDescription = nil")
+                return
+            }
+            
+            print("token: \(user.uid)")
+            self.openRootView()
         }
+    }
+    
+    func createUserAccount(login: String, email: String, password: String) {
+        
+        showLoadingView()
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            
+            self.hideLoadingView()
+            
+            guard let user = result?.user, error == nil else {
+                print("error: \(error?.localizedDescription ?? "error")")
+                self.showAlert(massage: error?.localizedDescription ?? "localizedDescription = nil")
+                return
+            }
+            
+            print("token: \(user.uid)")
+            self.openRootView()
+        }
+    }
+    
+    func checkFieldsAndMakeLoginLogic() {
+        
+        guard let login = loginField.text, !login.isEmpty  else {
+            showAlert(massage: "заполните логин")
+            return
+        }
+        guard let email = emailField.text, !email.isEmpty else {
+            showAlert(massage: "заполните email")
+            return
+        }
+        guard let password = passwordField.text, !password.isEmpty else {
+            showAlert(massage: "заполните passwodr")
+            return
+        }
+        
+        if singUp {
+            createUserAccount(login: login, email: email, password: password)
+        } else {
+            loginUser(email: email, password: password)
+        }
+    }
+    
+    // MARK: UI
+    
+    func configureLoadingView() {
+        loadingView.color = .black
+        loadingView.translatesAutoresizingMaskIntoConstraints = false
+        blurEffectView.contentView.addSubview(loadingView)
+        loadingView.centerXAnchor.constraint(equalTo: blurEffectView.contentView.centerXAnchor).isActive = true
+        loadingView.centerYAnchor.constraint(equalTo: blurEffectView.contentView.centerYAnchor).isActive = true
+    }
+    
+    func configureBlurView() {
+        let blurEffect = UIBlurEffect(style: .dark)
+        blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(blurEffectView)
+    }
+    
+    func showLoadingView() {
+        loadingView.isHidden = false
+        loadingView.startAnimating()
+        
+        enterButton.isEnabled = false
+        singButton.isEnabled = false
+        
+        blurEffectView.isHidden = false
+    }
+    
+    func hideLoadingView() {
+        loadingView.isHidden = true
+        loadingView.stopAnimating()
+        
+        enterButton.isEnabled = true
+        singButton.isEnabled = true
+        
+        blurEffectView.isHidden = true
     }
     
     func showAlert(massage: String) {
@@ -52,55 +171,7 @@ class AuthViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    func createUserAccount(login: String, email: String, password: String) -> Bool {
-        
-        var resultBool = false
-        
-        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
-            if error == nil {
-                if let result = result {
-                    print("token: \(result.user.uid)")
-                    resultBool = true
-                }
-            } else {
-                self.showAlert(massage: error?.localizedDescription ?? "localizedDescription = nil")
-                resultBool = false
-            }
-        }
-        return resultBool
-    }
-    
-    func checkFieldsAndCreateUser() -> Bool {
-        
-        let login = loginField.text!
-        let email = emailField.text!
-        let password = passwordField.text!
-        
-        var userBool = false
-        
-        if (singUp) {
-            
-            if (!login.isEmpty && !email.isEmpty && !password.isEmpty) {
-                if createUserAccount(login: login, email: email, password: password) {
-                    userBool = true
-                }
-                
-            } else {
-                showAlert(massage: "заполните все поля")
-                userBool = false
-            }
-        } else {
-            if (!email.isEmpty && !password.isEmpty) {
-                
-                userBool = true
-            } else {
-                showAlert(massage: "заполните все поля")
-                userBool = false
-            }
-        }
-        return userBool
-    }
-    func navigationView() {
+    func openRootView() {
         
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
         let view = storyBoard.instantiateViewController(withIdentifier: "TabView") as! TabViewController
@@ -109,14 +180,18 @@ class AuthViewController: UIViewController {
         NavController.modalPresentationStyle = .fullScreen
         self.navigationController?.pushViewController(view, animated: true)
     }
+    
+    // MARK: Actions
+    
+    func switchButtonAction() {
+        checkFieldsAndMakeLoginLogic()
+    }
 }
 
 extension AuthViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
-        if checkFieldsAndCreateUser() {
-            navigationView()
-        }
+        checkFieldsAndMakeLoginLogic()
         
         return true
     }
--- CoockS/TabViewController.swift
+++ CoockS/TabViewController.swift
@@ -8,23 +8,12 @@
 
 import UIKit
 
-class TabViewController: UITabBarController {
+class TabViewController: UITabBarController, UITabBarControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.delegate = self
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
